82f4d9b8da8fe4d57391d4d624daa23da3fc84f1
TomSmithCGAT <tss38@cam.ac.uk>
2018-07-18 15:26:19 +0100

    specifically installing p3.5.1 for OSX to avoid pysam error

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,7 +2,7 @@
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
     brew update
-    brew upgrade python
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
     brew install freetype
     pip3 install cython
     pip3 install pandas==0.19
